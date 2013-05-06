5d2c6183da0f690dce3125d1423ff5b14136a00b
Pascal Essiembre <pascal.essiembre@norconex.com>
2013-05-05 21:54:37 -0400

    Several tuning and fixes after running Sonar.

---
+++ norconex-commons-lang/src/main/java/com/norconex/commons/lang/io/StreamException.java
@@ -0,0 +1,43 @@
+/* Copyright 2010-2013 Norconex Inc.
+ * 
+ * This file is part of Norconex Commons Lang.
+ * 
+ * Norconex Commons Lang is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Norconex Commons Lang is distributed in the hope that it will be useful, 
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Norconex Commons Lang. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.norconex.commons.lang.io;
+
+/**
+ * Runtime exception when dealing with I/O streams.
+ * @author <a href="mailto:pascal.essiembre@norconex.com">Pascal Essiembre</a>
+ */
+public class StreamException extends RuntimeException {
+
+    private static final long serialVersionUID = 2988235417669737473L;
+
+    public StreamException() {
+    }
+
+    public StreamException(String message) {
+        super(message);
+    }
+
+    public StreamException(Throwable cause) {
+        super(cause);
+    }
+
+    public StreamException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
