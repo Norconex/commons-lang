89b7bf08114ee2504dcaaac6060a417442fe6153
Pascal Essiembre <pascal.essiembre@norconex.com>
2014-04-20 23:28:08 -0400

    Removed remaining dots before some ContentType extensions.  Fixed ContentType test.

--- norconex-commons-lang/src/test/java/com/norconex/commons/lang/file/ContentTypeTest.java
+++ norconex-commons-lang/src/test/java/com/norconex/commons/lang/file/ContentTypeTest.java
@@ -12,10 +12,10 @@ public class ContentTypeTest {
         Assert.assertEquals("Open eBook Publication Structure", 
                 ContentType.valueOf(
                         "application/oebps-package+xml").getDisplayName());
-        Assert.assertEquals(".pdf", ContentType.PDF.getExtension());
-        Assert.assertEquals(".wpd", 
+        Assert.assertEquals("pdf", ContentType.PDF.getExtension());
+        Assert.assertEquals("wpd", 
                 ContentType.valueOf("application/wordperfect").getExtension());
-        Assert.assertArrayEquals(new String[]{ ".wpd", ".wp", ".wp5", ".wp6" },
+        Assert.assertArrayEquals(new String[]{ "wpd", "wp", "wp5", "wp6" },
                 ContentType.valueOf("application/wordperfect").getExtensions());
     }
 }
