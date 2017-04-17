5817a359dc2863a49f69a3842774d958eb16f38c
Pascal Essiembre <pascal.essiembre@norconex.com>
2017-04-16 22:58:22 -0400

    Install script tweaking.

--- norconex-commons-lang/src/main/assembly/install.sh
+++ norconex-commons-lang/src/main/assembly/install.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+# Copyright 2017 Norconex Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# 
+#    http://www.apache.org/licenses/LICENSE-2.0
+# 
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 cd $(dirname $0)
 
 echo ""
@@ -8,7 +21,7 @@ echo "To install this component and its dependencies into another product,"
 echo "please specify the target product directory where libraries (.jar files)"
 echo "can be found."
 echo ""
-echo "This is often a "lib" directory. For example, to install this component"
+echo "This is often a \"lib\" directory. For example, to install this component"
 echo "into the Norconex HTTP Collector, specify the full path to the Collector"
 echo "\"lib\" directory, which may look somewhat like this:"
 echo ""
