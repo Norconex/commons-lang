da58e9b3dd721af52836c0c199dfca8969ab2996
Pascal Essiembre <pascal.essiembre@norconex.com>
2023-07-09 23:55:21 -0400

    Now a Java Module.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,17 @@
-
+/* Copyright 2023 Norconex Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 @SuppressWarnings({ "module" })
 module com.norconex.commons.lang {
     exports com.norconex.commons.lang.map;
@@ -28,8 +41,6 @@ module com.norconex.commons.lang {
     exports com.norconex.commons.lang.xml.flow.impl;
     exports com.norconex.commons.lang.net;
 
-    // Mark as "transitive" the ones that are publicly exposed to
-    // this module consumers.
     requires com.fasterxml.jackson.annotation;
     requires icu4j;
     requires imgscalr.lib;
